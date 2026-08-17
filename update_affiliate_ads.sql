d5dd4848ed1b3535005bc5ed0c24291b5efe2e47
Jonatan Moreira <jonatandanielmoreira@gmail.com>
2026-08-16 21:28:51 -0300

    feat: configurado enlace oficial de afiliado de Mercado Libre https://meli.la/1fwfx2Y en la tabla ads de D1

---
+++ update_affiliate_ads.sql
@@ -0,0 +1,75 @@
+UPDATE ads SET redirect_url = 'https://meli.la/1fwfx2Y';
+
+INSERT OR REPLACE INTO ads (id, title, subtitle, image_url, redirect_url, color, border, text_color, display_order, is_active) VALUES
+(
+  'meli-ofertas-01',
+  'Mercado Libre',
+  '⚡ Ofertas Exclusivas hasta 40% OFF y Envíos Rápidos',
+  'https://http2.mlstatic.com/storage/splinter-admin/o:f_webp,q_auto:best/1690000000000-banner.jpg',
+  'https://meli.la/1fwfx2Y',
+  '#FFE600',
+  '#E6CF00',
+  '#2D3277',
+  1,
+  1
+),
+(
+  'meli-powerbanks-02',
+  'Power Banks y Cargadores',
+  '🔋 No te quedes sin batería mientras viajás',
+  NULL,
+  'https://meli.la/1fwfx2Y',
+  '#FFE600',
+  '#E6CF00',
+  '#2D3277',
+  2,
+  1
+),
+(
+  'meli-sube-03',
+  'Fundas y Accesorios SUBE',
+  '💳 Fundas protectoras, llaveros y tarjeteros',
+  NULL,
+  'https://meli.la/1fwfx2Y',
+  '#FFE600',
+  '#E6CF00',
+  '#2D3277',
+  3,
+  1
+),
+(
+  'meli-auriculares-04',
+  'Auriculares Inalámbricos',
+  '🎧 Disfrutá tu viaje con la mejor música',
+  NULL,
+  'https://meli.la/1fwfx2Y',
+  '#FFE600',
+  '#E6CF00',
+  '#2D3277',
+  4,
+  1
+),
+(
+  'meli-mochilas-05',
+  'Mochilas Urbanas Antirrobo',
+  '🎒 Impermeables y con puerto de carga USB',
+  NULL,
+  'https://meli.la/1fwfx2Y',
+  '#FFE600',
+  '#E6CF00',
+  '#2D3277',
+  5,
+  1
+),
+(
+  'meli-supermercado-06',
+  'Mercado Libre Supermercado',
+  '🛒 Comprá en el camino y recibilo en tu casa',
+  NULL,
+  'https://meli.la/1fwfx2Y',
+  '#FFE600',
+  '#E6CF00',
+  '#2D3277',
+  6,
+  1
+);
