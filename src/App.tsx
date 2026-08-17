e420fbe08184bc0329e9109089edcc80c8e46dfa
Jonatan Moreira <jonatandanielmoreira@gmail.com>
2026-08-17 00:10:32 -0300

    feat: desplegar en produccion version final con columna de Mercado Libre y redes sociales en bottom: 26px

--- src/App.tsx
+++ src/App.tsx
@@ -4346,7 +4346,7 @@ function App() {
 
         <div style={{
           position: 'absolute',
-          bottom: '12px',
+          bottom: '26px',
           right: '16px',
           zIndex: 1100,
           pointerEvents: 'auto',
